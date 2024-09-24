040e5784e5ed237ae2c74206ab89fa76bbf813c2
Éverllon Souza <147229679+DevEverllon@users.noreply.github.com>
2024-09-24 00:54:34 -0300

    update

--- faq.js
+++ faq.js
@@ -4,5 +4,8 @@ accordions.forEach(accordions => {
     accordions.addEventListener('click', () =>{
         const body = accordions.querySelector('.accordion-body');
         body.classList.toggle('active');
+        
+        const header = accordions.querySelector('.accordion-header');
+        header.classList.toggle('active');
     })
 })
