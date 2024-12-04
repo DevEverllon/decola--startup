c4aa75b227a1bf1f0b2d42e48b5883c262a610c1
Éverllon Souza <147229679+DevEverllon@users.noreply.github.com>
2024-12-04 02:13:22 -0300

    update

---
+++ scroll.js
@@ -0,0 +1,30 @@
+// ANIMAÇÃO DE SCROLL
+
+// Menu
+ScrollReveal().reveal('header', {
+    origin: 'top',
+    duration: 1000,
+    distance: '60%',
+});
+
+// Animação Informações Header
+ScrollReveal().reveal('.infos-home', {
+    origin: 'left',
+    duration: 2000,
+    distance: '10%',
+});
+
+// Animação Imagem Header
+ScrollReveal().reveal('.image--descktop', {
+    origin: 'rigth',
+    duration: 2000,
+    distance: '10%',
+});
+
+// Animação Card - Section 'Mission'
+ScrollReveal().reveal('.card-mission', {
+    origin: 'left',
+    duration: 2000,
+    distance: '80%',
+});
+
